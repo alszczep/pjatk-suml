f2f24835a99fd1f10ac8ae5df599934e33ff76dd
Mychnik <mmanowiecki01@gmail.com>
2025-05-30 22:11:32 +0200

    Applied pre commit formatting

--- backend/catsdogs/blobStorage.py
+++ backend/catsdogs/blobStorage.py
@@ -1,13 +1,17 @@
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+from datetime import datetime, timedelta
+import io
 import os
 import uuid
-import io
-from datetime import datetime, timedelta
 
-service_client = BlobServiceClient.from_connection_string(os.environ["BLOB_STORAGE_CONNECTION_STRING"])
+from azure.storage.blob import BlobSasPermissions, BlobServiceClient, generate_blob_sas
+
+service_client = BlobServiceClient.from_connection_string(
+    os.environ["BLOB_CONNECTION_STRING"]
+)
 model_container_client = service_client.get_container_client("model")
 images_container_name = "images"
 images_container_client = service_client.get_container_client(images_container_name)
+
 
 def upload_image_to_blob(file):
     extension = os.path.splitext(file.name)[-1]
@@ -19,10 +23,12 @@ def upload_image_to_blob(file):
 
     return blob_client.url
 
+
 def download_model_from_blob():
     blob_client = model_container_client.get_blob_client("model.keras")
     data = blob_client.download_blob().readall()
     return io.BytesIO(data)
+
 
 def parse_connection_string(connection_string):
     parts = connection_string.split(";")
@@ -33,10 +39,13 @@ def parse_connection_string(connection_string):
             result[key] = value
     return result
 
+
 def get_blob_file_url_with_sas_token(file_url):
     blob_name = file_url.split("/")[-1]
 
-    parsed_connection_string = parse_connection_string(os.environ["BLOB_STORAGE_CONNECTION_STRING"])
+    parsed_connection_string = parse_connection_string(
+        os.environ["BLOB_CONNECTION_STRING"]
+    )
     account_name = parsed_connection_string["AccountName"]
     account_key = parsed_connection_string["AccountKey"]
 
